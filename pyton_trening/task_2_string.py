580908584888335d9253b5a6487a091dfee819c9
Yrucha <yrucha@mail.ru>
2023-04-21 20:43:22 +0300

    first commit

---
+++ pyton_trening/task_2_string.py
@@ -0,0 +1,11 @@
+s = "Это строка"
+print(s + '\n')
+g = '''(a[-2]) # t)
+трока '''
+print(g + '\n')
+print( 'Строки' + 'можно ' + 'складывать')
+print ('Auto' * 3)
+print(len('test'))
+a = 'Auto'
+print(a[0]) # A
+print(a[-2]) # t
---
+++ pyton_trening/task_6_function.py
@@ -0,0 +1,55 @@
+# определение функции
+def add(x, y):
+ return x + y
+
+
+# вызываем функцию
+print(arg(1, 2))
+
+
+# вызываем функцию с другими аргументами
+print(arg(a', 'm tester'))
+ return a + b + c + d
+
+
+ def arg(a, b, c=2, d=3):
+     return a + b + c + d
+
+
+print(arg(1, 1, 1, 1))
+
+
+print(arg(2, 2))
+
+print(arg(2, 2, '1', 1))
+
+
+def range_arg(a, b, c d):
+    return a + ' ' + b + ' ' + c + ' '  + d
+
+
+print(arg 1)
+
+
+
+def test (a = (1, 2, 3, 4)):
+    return a[0]
+test()
+
+
+def test2 (radius, pi=3,14159):
+    return pi * radius * radius
+
+
+print(test2(2))
+
+
+
+
+
+
+
+
+
+
+
